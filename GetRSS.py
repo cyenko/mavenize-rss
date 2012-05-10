6ef65ddb3825c4bd125dbce697f11f2ebac0cdad
Christian Yenko <chris@mavenize.me>
2012-05-10 17:15:46 -0500

    Added alternative parsing through <link> tag, added lump text function.  Next: getSummaryText function

--- GetRSS.py
+++ GetRSS.py
@@ -3,30 +3,29 @@ from bs4 import BeautifulSoup as soup
 class GetRSS:
     print('tes');
     def __init__(self):
-        self.alchemyObject = AlchemyAPI.AlchemyAPI();
-        self.alchemyObject.loadAPIKey("api_key.txt");
-        print('This script retrieves a list of permalinks, given an RSS URL');
-        #self.url=raw_input('Enter URL: ');
-        #print('You entered ' + self.url);
-        #Change the below to whatever URL we need.  Can be interfaced with a get method
+        self.alchemyObject = AlchemyAPI.AlchemyAPI()
+        self.alchemyObject.loadAPIKey("api_key.txt")
+        print('Key loaded successfully.')
+        #Provided a sample URL, in case you want to test it
         self.url = 'http://www.rottentomatoes.com/syndication/rss/top_news.xml'
-        #self.urllist = self.getPermaLinks(self.url);
-        #result = alchemyObject.URLGetRawText(url);
-        #print(result);
-        #print('done');
-	    
+        print('Script loaded sucessfully.')
+
     def getPermaLinks(self,url):
         print('called helper method')
         req = urllib2.Request(url, headers={'User-Agent' : "Magic Browser"})
-        page = urllib2.urlopen(req) #We are blocked from accessing feeds (detected as bot
+        page = urllib2.urlopen(req) #We are blocked from accessing feeds (detected as bot)
+        #I foresee the above being large-scale inefficient.  Any ideas?
         soupPage = soup(page)
         returnList = []
         unparsedList = soupPage.findAll('guid')
+        if not unparsedList: #Some RSS pages use the 'link' tag rather than the 'guid' tag
+            unparsedList = soupPage.findAll('link')
         for listelement in unparsedList:
 	        returnList.append(listelement.findAll(text=True))
         return returnList
 
-    def getLumpText(self,url):
+    def getLumpText(self,url): #We strip the navigation links and get only content.
+        #Should we leave in \n and \t marks, or no?
         print('Getting Lump Text');
-        returnText = self.alchemyObject.URLGetRawText(url)
-        return returnText
+        returnText = self.alchemyObject.URLGetText(url)
+        return returnText 
